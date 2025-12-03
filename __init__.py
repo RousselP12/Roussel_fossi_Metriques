14ffa5153e4b69953d09335dd7dbc547e42425a5
Roussel FOSSI <116687896+RousselP12@users.noreply.github.com>
2025-12-03 12:21:02 +0100

    Add contact and weather routes to the Flask app

--- __init__.py
+++ __init__.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')  #COMMENTAIRE2
+  
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>" 
+  
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -24,5 +26,9 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 if __name__ == "__main__":
   app.run(debug=True)
