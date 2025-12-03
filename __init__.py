a40e0ced62688eeebcaa6b57f9a120786e6b8fc5
Roussel FOSSI <116687896+RousselP12@users.noreply.github.com>
2025-12-03 12:39:41 +0100

    Fix route for histogramme to remove file extension

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.route("/histogramme.html/")
+@app.route("/histogramme")
 def mongraphique():
     return render_template("histogramme.html")
 if __name__ == "__main__":
